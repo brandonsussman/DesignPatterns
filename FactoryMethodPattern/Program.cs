2907e7e786c1b6e407e50d0a48926966dd2c6a8f
brandonsussman <brandonsussman23@gmail.com>
2023-10-24 17:03:26 +0200

    FactoryPattern

---
+++ FactoryMethodPattern/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using System.Diagnostics.Contracts;
+using System.Net.WebSockets;
+using System.Runtime.InteropServices;
+
+Factory factoryA = new FactoryA();
+Product productA = factoryA.CreateProduct();
+Console.WriteLine(productA.display());
+
+
+Factory factoryB= new FactoryB();
+Product productB = factoryB.CreateProduct();
+Console.WriteLine(productB.display());
+
+abstract class Product
+{
+
+    public abstract string display();
+}
+
+class ConcreteProductA :Product
+{
+    public override string display()
+    {
+        return "This is product A";
+    }
+}
+
+class ConcreteProductB : Product
+{
+    public override string display()
+    {
+        return "This is product B";
+    }
+}
+
+
+abstract class Factory 
+{
+    public abstract Product CreateProduct();    
+}
+
+
+class FactoryA : Factory {
+    public override Product CreateProduct() { return new ConcreteProductA(); }
+}
+
+
+class FactoryB : Factory
+{
+    public override Product CreateProduct() { return new ConcreteProductB(); }
+}
