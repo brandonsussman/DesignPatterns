6f0523d1e74c74d0d72fac9701e129e8e61c9891
brandonsussman <brandonsussman23@gmail.com>
2023-10-25 13:13:10 +0200

    Adapter Pattern

---
+++ AdapterMethod/Program.cs
@@ -0,0 +1,57 @@
+﻿SquarePeg squarePeg = new SquarePeg(3);
+RoundPeg adaptedPeg = new SquaretoRoundPegAdapter(squarePeg);
+RoundHole hole = new RoundHole(4);
+hole.fits(adaptedPeg);
+
+    public  class RoundPeg
+{
+   public int radius { get;  }
+    public RoundPeg(int radius)
+    {
+        this.radius= radius;
+    }
+
+   
+
+}
+
+public class SquarePeg
+{
+    public int width { get; }
+
+   public SquarePeg(int width) { this.width = width; }
+}
+
+
+public class SquaretoRoundPegAdapter: RoundPeg
+{
+    private SquarePeg squarePeg;
+    public SquaretoRoundPegAdapter(SquarePeg squarePeg):base(squarePeg.width)
+    {
+        this.squarePeg = squarePeg;
+    }
+
+}
+
+
+public class RoundHole
+{
+    public int radius { get; }
+   public RoundHole(int radius) { this.radius = radius; }
+
+   
+
+    public void fits(RoundPeg peg)
+    {
+        if (radius >= peg.radius)
+        {
+            Console.WriteLine("fits");
+        }
+        else
+        {
+            Console.WriteLine("Doesn't fit");
+        }
+    }
+}
+
+
